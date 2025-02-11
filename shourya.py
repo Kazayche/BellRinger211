7e14fe648e58b23836a2f8a9e1f8f4ada9659d71
Shourya Pattabhi <96268277+Kazayche@users.noreply.github.com>
2025-02-11 14:58:07 -0800

    Create shourya.py

---
+++ shourya.py
@@ -0,0 +1,6 @@
+import datetime
+date=int(input(" day in the month"))
+time=int(input("hour in the day "))
+time_left=14-date
+hours_left=(time*24)-time
+print(hours_left,"left till your desired date ")
